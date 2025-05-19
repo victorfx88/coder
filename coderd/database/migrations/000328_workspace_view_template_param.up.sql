e11bd084cb59ceb41dabd6758580090167bbce7f
Steven Masley <stevenmasley@gmail.com>
2025-05-19 16:26:44 -0500

    chore: add use_classic_param_flow to workspace response

---
+++ coderd/database/migrations/000328_workspace_view_template_param.up.sql
@@ -0,0 +1,37 @@
+-- Recreate view to include new template setting
+DROP VIEW workspaces_expanded;
+
+CREATE VIEW
+	workspaces_expanded
+AS
+SELECT
+	workspaces.*,
+	-- Owner
+	visible_users.avatar_url AS owner_avatar_url,
+	visible_users.username AS owner_username,
+	-- Organization
+	organizations.name AS organization_name,
+	organizations.display_name AS organization_display_name,
+	organizations.icon AS organization_icon,
+	organizations.description AS organization_description,
+	-- Template
+	templates.name AS template_name,
+	templates.display_name AS template_display_name,
+	templates.icon AS template_icon,
+	templates.description AS template_description,
+	templates.use_classic_parameter_flow as template_use_classic_parameter_flow
+FROM
+	workspaces
+		INNER JOIN
+	visible_users
+	ON
+		workspaces.owner_id = visible_users.id
+		INNER JOIN
+	organizations
+	ON workspaces.organization_id = organizations.id
+		INNER JOIN
+	templates
+	ON workspaces.template_id = templates.id
+;
+
+COMMENT ON VIEW workspaces_expanded IS 'Joins in the display name information such as username, avatar, and organization name.';
